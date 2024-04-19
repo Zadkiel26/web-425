8553e86e13d50c13fe4442abb0d5248b2a21b702
Zadkiel <zadkiel679@gmail.com>
2024-04-19 14:35:32 -0400

    Exercise 5.2 - Navigation and Layout - Done

---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: book-list.component.ts
+ * Author: Zadkiel Rodriguez Alvarado
+ * Date: 4/19/2024
+ * Description: Book list component
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
